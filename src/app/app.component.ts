383126e35918fa8d7c08891943e5666b54f0720a
pramod1 <pramodkumarw@gmail.com>
2018-05-31 00:26:48 +0530

    done by pramod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Router, Event, NavigationStart,NavigationEnd,NavigationCancel,NavigationError } from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  loading:boolean=true;
+  id:number=333;
+  constructor(readonly route:Router) {
+   }
+
+   checkRouterEvent(routeEvent:Event):void{
+  }
+   
+  ngOnInit() {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
-
+import { WelcomeComponent } from "../app/welcome/welcome.component";
+import { ProductListComponent } from "../app/productlist/productlist.component";
+import { ProductDetailComponent } from "../app/productdetail/productdetail.component";
+import { NotfoundComponent } from "../app/notfound/notfound.component";
+import { AboutguardGuard } from './aboutguard.guard';
+import { HttpModule  } from "@angular/http";
+ import { AppRoutingModule } from "./routing/appRouting.module";
+import { DataService } from "./mythings/data.service";
+import { ProductEditComponent } from "../app/product-edit/product-edit.component";
+import {ProductEditInfoComponent  } from "../app/product-edit-info/product-edit-info.component";
+import { ProdcutListResolve } from "./mythings/productlist-Resolve.service";
+import { AuthService } from "./mythings/auth.service";
+import {  AuthGuard} from "./mythings/auth-guard.guard";
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    NotfoundComponent,
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpModule,FormsModule
+   ,AppRoutingModule
   ],
-  providers: [],
+  providers: [ProdcutListResolve,DataService,AuthService,AuthGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ }
+
+
+
